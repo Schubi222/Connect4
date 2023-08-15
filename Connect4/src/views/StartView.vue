5530794e86f8a01268fe125b5af9744b75babb8f
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2023-08-15 15:38:12 +0200

    Home View (does not differentiate between cpu and player) 44min

--- Connect4/src/views/StartView.vue
+++ Connect4/src/views/StartView.vue
@@ -1,10 +1,21 @@
 <template>
-  <main>
-    <img src="@/assets/svg/pink_smiley_token.svg" alt="">
-    <img src="@/assets/svg/pink_token.svg" alt="">
-  </main>
+  <div class="Home">
+    <div class="top-logo">
+      <img src="@/assets/svg/pink_token.svg" alt="">
+      <img src="@/assets/svg/yellow_token.svg" alt="">
+      <img src="@/assets/svg/yellow_token.svg" alt="">
+      <img src="@/assets/svg/pink_token.svg" alt="">
+    </div>
+    <router-link class="pink_btn" :to="{name:'Play'}">play vs cpu</router-link>
+    <router-link class="yellow_btn" :to="{name:'Play'}">play vs player</router-link>
+    <router-link class="white_btn" :to="{name:'Rules'}">game rules</router-link>
+  </div>
 </template>
 
 <script setup lang="ts">
 
 </script>
+
+<style scoped>
+@import "@/assets/StartView.css";
+</style>
