ba6a4faacf8a627d8796b3112ecb2b73733ff5e2
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2023-08-16 11:53:31 +0200

    rules 20min

--- Connect4/src/views/RulesView.vue
+++ Connect4/src/views/RulesView.vue
@@ -1,11 +1,25 @@
 <template>
-RULES
+  <div class="Rules-Wrapper">
+    <h1>Rules</h1>
+    <h2>Objective</h2>
+    <p>Be the first player to connect 4 of the same colored discs in a row (either vertically, horizontally, or diagonally):</p>
+    <h2>How to play</h2>
+    <ol>
+      <li>Red goes first in the first game</li>
+      <li>Players must alternate turns, and only one disc can be dropped in each turn.</li>
+      <li>The game ends when there is a 4-in-a-row or a stalemate.</li>
+      <li>The starter of the previous game goes second on the next game.</li>
+    </ol>
+    <img src="@/assets/svg/accept_btn.svg" alt="accept" @click="router.push({name:'Start'})">
+  </div>
+
 </template>
 
 <script setup lang="ts">
 
+import router from "@/router";
 </script>
 
 <style scoped>
-
+@import "@/assets/RulesView.css";
 </style>
